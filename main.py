8f13e74317e3140c396a298588f22d8db8378ac6
ahaanmybot <ahaanbinaj@gmail.com>
2026-03-27 16:34:52 +0400

    Implement tuple product calculation
    Fix typo in function call and syntax error in for loop.

---
+++ main.py
@@ -0,0 +1,12 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+def tuple_product(t):
+  product = 1
+  for num in t
+      product *= num
+  return product
+result1 = tuple_product(tup1)
+result2 = tuble_product(tup2)
+
+print("product of tup1:", result1)
+print("product of tup2;", result
